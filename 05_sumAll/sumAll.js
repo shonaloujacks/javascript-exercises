4753b9be58e8d9a6288d4f575c7594827feb7953
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-06-19 16:55:23 +0100

    Created conditional loop to calculate difference between 2 numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+  let sum = 0;
 
+  if (number1 < 0 || number2 < 0) {
+    return "ERROR";
+  } else if (typeof number1 === "string" || typeof number2 === "string") {
+    return "ERROR";
+  } else if (Array.isArray(number1) || Array.isArray(number2)) {
+    return "ERROR";
+  } else if (number1 < number2) {
+    for (number1; number1 <= number2; number1++) {
+      sum += number1;
+    }
+    return sum;
+  } else if (number2 < number1) {
+    for (number2; number2 <= number1; number2++) {
+      sum += number2;
+    }
+    return sum;
+  }
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
