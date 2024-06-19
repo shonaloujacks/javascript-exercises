9f1284e918134a489262f02d392cc2de63af0e4a
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-06-19 13:34:26 +0100

    Created function to filter elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...args) {
+    function keepElement(number) {
+        if (args.includes(number)) {
+            return false;
+        }   else return true;
+    }
+    const filteredElements = inputArray.filter(keepElement);
+
+    return filteredElements;
 };
+
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
