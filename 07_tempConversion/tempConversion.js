5e175bcf03dfc50cbfa61907bcfdefdcf93a172c
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-06-19 18:00:00 +0100

    created two functions to convert celsius to  farenehit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  if (Number.isInteger(celsius)) {
+    return Number(celsius);
+  } else return Number(celsius.toFixed(1));
+};
+convertToCelsius(32);
+
+const convertToFahrenheit = function (celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  if (Number.isInteger(fahrenheit)) {
+    return Number(fahrenheit);
+  } else return Number(fahrenheit.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
-};
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
