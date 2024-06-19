5da664b00b427adead7736804e5570d1044f0259
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-06-19 11:58:27 +0100

    Created for loop and condition statement for repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = repeatString('hey', 3) {
-
+const repeatString = (word, times) => {
+    let string = "";
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        string += word; 
+    }
+ return string;
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
