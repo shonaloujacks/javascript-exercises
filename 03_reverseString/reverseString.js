7c0b4f4e20f8ffa8d62e149c7a97f569690afd6a
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-06-19 12:28:53 +0100

    created reverseString function to reverse string input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+let reversed = "";
+reversed = word.split("").reverse().join(""); 
+return reversed;
 };
+
+reverseString("Hello world");
 
 // Do not edit below this line
 module.exports = reverseString;
