f446ea2cc3df2044c2a4183c16bb3a0bb84a28e7
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-07-24 17:35:37 +0100

    calculator js code added

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,35 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b; 
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((sum, current) => sum + current, 0);
 	
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a**b;
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for (a; a > 0 ; a--) {
+    total *= a; 
+  }
+  return total;
 };
+
 
 // Do not edit below this line
 module.exports = {
