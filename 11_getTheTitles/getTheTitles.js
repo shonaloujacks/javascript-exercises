7656d2ada115b81c598bedeba4ba4467f1b48071
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-07-25 15:47:01 +0100

    Created function to map book titles into new array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+const arrayOfTitles = books.map(book => book.title);
+
+    return arrayOfTitles;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
