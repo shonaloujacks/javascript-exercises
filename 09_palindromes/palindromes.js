d0c8490eaa0c19b3bc9b6ecacb46cf5be1d05512
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-07-25 10:10:15 +0100

    Created function to test for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let stringBackward =  string.toCharArray().reverse().toString();
+
+  if (string === stringBackward) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+
+  console.log(palindromes('racecar'))
+    // create 2 variables, stringForward and stringBackward. stringBackward convert string to an array and should apply the reverse method to stringForward. Then convert back to a string.
+    // Then write a comparison (ternery operator?) that compares the two and looks for strict equality. If equal, return true, if not equal return false
 
 };
 
