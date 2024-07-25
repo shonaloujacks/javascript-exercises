f6f0ad64a710fb3125e54dcda0a8f191b281d63d
shonaloujacks <98349286+shonaloujacks@users.noreply.github.com>
2024-07-25 13:10:15 +0100

    Created a function that delivers a number at a particular index in the Fibonacci sequence

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,21 @@
 const palindromes = function (string) {
-  let stringBackward =  string.toCharArray().reverse().toString();
 
-  if (string === stringBackward) {
-    return true;
-  } 
-  else {
-    return false;
-  }
+    let refinedString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reversedString =  refinedString
+        .split('')
+        .reverse()
+        .join('');
 
-  console.log(palindromes('racecar'))
+    return refinedString === reversedString; 
+
+
+};
+
+
+
     // create 2 variables, stringForward and stringBackward. stringBackward convert string to an array and should apply the reverse method to stringForward. Then convert back to a string.
     // Then write a comparison (ternery operator?) that compares the two and looks for strict equality. If equal, return true, if not equal return false
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    const fibonacciSequence = [1, 1];
+    let index = 0;
+    for (number; index<number-2; index++) {
+        fibonacciSequence.push(fibonacciSequence[index] + fibonacciSequence[index+1]);
+    }
+    return fibonacciSequence[number-1]
 
 };
+
+// Add the two preceeding numbers to return their sum as the next number in the sequence and then store that number in an arary. 
+// Repeat for the next two preceeding numbers
 
 // Do not edit below this line
 module.exports = fibonacci;
